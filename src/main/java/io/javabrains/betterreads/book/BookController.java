0d8e8ab1354c8079d3686f7894b007236aa7ba29
Imran Hossain <ih060899@users.noreply.github.com>
2022-02-02 19:54:06 +0600

    Complete

--- src/main/java/io/javabrains/betterreads/book/BookController.java
+++ src/main/java/io/javabrains/betterreads/book/BookController.java
@@ -3,10 +3,16 @@ package io.javabrains.betterreads.book;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import io.javabrains.betterreads.userbooks.UserBooks;
+import io.javabrains.betterreads.userbooks.UserBooksPrimaryKey;
+import io.javabrains.betterreads.userbooks.UserBooksRepository;
 
 @Controller
 public class BookController {
@@ -16,17 +22,40 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
 
-    @GetMapping("/books/{bookId}")
-    public String getBook(@PathVariable String bookId, Model model) {
+    @Autowired
+    UserBooksRepository userBooksRepositoy;
+    // @GetMapping(value = "/books")
+    // public String getBook() {
+    // System.out.println("hello");
+    // return "hello";
+    // }
+
+    @GetMapping(value = "/books/{bookId}")
+    public String getBook(@PathVariable String bookId, Model model, @AuthenticationPrincipal OAuth2User principal) {
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if (optionalBook.isPresent()) {
             Book book = optionalBook.get();
             String coverImageUrl = "/images/no-image.png";
             if (book.getCoverIds() != null && book.getCoverIds().size() > 0) {
                 coverImageUrl = COVER_IMAGE_ROOT + book.getCoverIds().get(0) + "-L.jpg";
-                model.addAttribute("coverImage", coverImageUrl);
             }
+            model.addAttribute("coverImage", coverImageUrl);
             model.addAttribute("book", book);
+
+            if (principal != null && principal.getAttribute("login") != null) {
+                String userId = principal.getAttribute("login");
+                model.addAttribute("loginId", userId);
+                UserBooksPrimaryKey userBooksPrimaryKey = new UserBooksPrimaryKey();
+                userBooksPrimaryKey.setBookId(bookId);
+                userBooksPrimaryKey.setUserId(userId);
+                Optional<UserBooks> userBooks = userBooksRepositoy.findById(userBooksPrimaryKey);
+                if (userBooks.isPresent()) {
+                    model.addAttribute("userBooks", userBooks.get());
+                } else {
+                    model.addAttribute("userBooks", new UserBooks());
+                }
+            }
+
             return "book";
         }
         return "book-not-found";
